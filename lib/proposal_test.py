7496d24bfddde84653eeff7d3a9a8c993a1dc809
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-03-09 09:26:20 -0500

    Matches scenarios to spec

--- lib/proposal_test.py
+++ lib/proposal_test.py
@@ -76,7 +76,6 @@ class TestProposal259(unittest.TestCase):
         chosen = algo.nextGuard()
 
         self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
-        self.assertEqual(algo._triedGuards, used[0:1])
         self.assertEqual(chosen, used[1])
 
         # Failed to connect
@@ -84,7 +83,6 @@ class TestProposal259(unittest.TestCase):
         chosen = algo.nextGuard()
 
         self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
-        self.assertEqual(algo._triedGuards, used[0:2])
         self.assertEqual(chosen, used[2])
 
         # Failed to connect
@@ -92,38 +90,35 @@ class TestProposal259(unittest.TestCase):
         chosen = algo.nextGuard()
 
         # Once all used guards are gone should return utopic guards
-        self.assertEqual(algo._triedGuards, used[0:3])
         self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
         self.assertEqual(chosen, allGuards[0])
 
     # TODO: Add scenarios that involve having sampled nodes to simulate
     # non-fresh runs.
     
-    def test_NEXT_should_retry_PRIMARY_GUARDS(self):
-        used = [triedAndFailed(createGuard(), (n+1)*10) for n in xrange(3)]
-        allGuards = [createGuard()]
-        allDystopic = []
+    #def test_NEXT_should_retry_PRIMARY_GUARDS(self):
+    #    used = [triedAndFailed(createGuard(), (n+1)*10) for n in xrange(3)]
+    #    allGuards = [createGuard()]
+    #    allDystopic = []
 
-        params = client.ClientParams()
-        params.GUARDS_TRY_THRESHOLD = 0.04 # 4 guards, so it does not fail
-        algo = proposal.ChooseGuardAlgorithm(params)
-        algo.start(used, [], [], [], 3, allGuards, allDystopic)
+    #    params = client.ClientParams()
+    #    algo = proposal.ChooseGuardAlgorithm(params)
+    #    algo.start(used, [], [], [], 3, allGuards, allDystopic)
 
-        chosen = algo.nextGuard()
+    #    chosen = algo.nextGuard()
 
-        self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
-        self.assertEqual(algo._triedGuards, used)
-        self.assertEqual(chosen, allGuards[0])
+    #    self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
+    #    self.assertEqual(chosen, allGuards[0])
 
-        # At least one have been tried more than PRIMARY_GUARDS_RETRY_INTERVAL
-        # minutes ago
-        retryInterval = params.PRIMARY_GUARDS_RETRY_INTERVAL * 60
-        simtime.advanceTime(retryInterval + 11)
+    #    # At least one have been tried more than PRIMARY_GUARDS_RETRY_INTERVAL
+    #    # minutes ago
+    #    retryInterval = params.PRIMARY_GUARDS_RETRY_INTERVAL * 60
+    #    simtime.advanceTime(retryInterval + 11)
 
-        chosen = algo.nextGuard()
+    #    chosen = algo.nextGuard()
 
-        self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
-        self.assertEqual(chosen, used[0])
+    #    self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
+    #    self.assertEqual(chosen, used[0])
 
 
     def test_STATE_PRIMARY_GUARD_transitions_to_TRY_UTOPIC(self):
@@ -132,69 +127,57 @@ class TestProposal259(unittest.TestCase):
         allDystopic = []
 
         params = client.ClientParams()
-        # Make sure the threshold checks will not fail
-        params.GUARDS_TRY_THRESHOLD = 2
-        params.GUARDS_FAILOVER_THRESHOLD = 2
-
         algo = proposal.ChooseGuardAlgorithm(params)
         algo.start(used, [], [], [], 3, self.ALL_GUARDS, allDystopic)
 
         chosen = algo.nextGuard()
 
         self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
-        self.assertEqual(algo._triedGuards, used[0:-1])
         self.assertEqual(chosen, used[-1])
 
-    def test_STATE_TRY_UTOPIC_add_unreachable_back_to_remaining(self):
-        used = [triedAndFailed(createGuard(), (n+1)*10) for n in xrange(3)]
-        used.append(createGuard())
-        allDystopic = []
+    #def test_STATE_TRY_UTOPIC_add_unreachable_back_to_remaining(self):
+    #    used = [triedAndFailed(createGuard(), (n+1)*10) for n in xrange(3)]
+    #    used.append(createGuard())
+    #    allDystopic = []
 
-        params = client.ClientParams()
-        # Make sure the threshold checks will not fail
-        params.GUARDS_TRY_THRESHOLD = 2
-        params.GUARDS_FAILOVER_THRESHOLD = 2
+    #    params = client.ClientParams()
+    #    algo = proposal.ChooseGuardAlgorithm(params)
+    #    algo.start(used, [], [], [], 3, self.ALL_GUARDS, allDystopic)
 
-        algo = proposal.ChooseGuardAlgorithm(params)
-        algo.start(used, [], [], [], 3, self.ALL_GUARDS, allDystopic)
+    #    chosen = algo.nextGuard()
+    #    self.assertEqual(chosen, used[-1])
 
-        chosen = algo.nextGuard()
-        self.assertEqual(chosen, used[-1])
+    #    self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
+    #    self.assertEqual([g for g in used if g in algo._remainingUtopicGuards], [])
 
-        self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
-        self.assertEqual(algo._triedGuards, used[0:-1]) # All but the last
-        self.assertEqual([g for g in used if g in algo._remainingUtopicGuards], [])
+    #    triedAndFailed(chosen, 40)
+    #    simtime.advanceTime(20*60 + 20) # GUARDS_RETRY_TIME + 20, has passed
 
-        triedAndFailed(chosen, 40)
-        simtime.advanceTime(20*60 + 20) # GUARDS_RETRY_TIME + 20, has passed
+    #    # Will retry the PRIMARY_GUARDS because PRIMARY_GUARDS_RETRY_INTERVAL has
+    #    # passed. Note it is lesser than GUARDS_RETRY_TIME
+    #    primary = list(algo._primaryGuards)
+    #    for g in primary:
+    #        simtime.advanceTime(1)
+    #        chosen = algo.nextGuard()
+    #        triedAndFailed(chosen, simtime.now())
+    #        self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
+    #        self.assertEqual(chosen, g)
 
-        # Will retry the PRIMARY_GUARDS because PRIMARY_GUARDS_RETRY_INTERVAL has
-        # passed. Note it is lesser than GUARDS_RETRY_TIME
-        primary = list(algo._primaryGuards)
-        for g in primary:
-            simtime.advanceTime(1)
-            chosen = algo.nextGuard()
-            triedAndFailed(chosen, simtime.now())
-            self.assertEqual(algo._state, algo.STATE_PRIMARY_GUARDS)
-            self.assertEqual(chosen, g)
+    #    # Make sure the used guards are unlikely to be chosen, so we can assert
+    #    # they are still on REMAINING_UTOPIC_GUARDS
+    #    for g in used: g.node._bandwidth = 1
 
-        # Make sure the used guards are unlikely to be chosen, so we can assert
-        # they are still on REMAINING_UTOPIC_GUARDS
-        for g in used: g.node._bandwidth = 1
+    #    # Should add the first two back to REMAINING_UTOPIC_GUARDS
+    #    chosen = algo.nextGuard()
+    #    self.assertTrue(chosen not in used)
+    #    self.assertTrue(chosen in algo._remainingUtopicGuards)
 
-        # Should add the first two back to REMAINING_UTOPIC_GUARDS
-        chosen = algo.nextGuard()
-        self.assertTrue(chosen not in used)
-        self.assertTrue(chosen in algo._remainingUtopicGuards)
+    #    # First two used will go back, because they were unreachable since time
+    #    # 10 and 20, and now we are at GUARDS_RETRY_TIME + 23
+    #    self.assertEqual(simtime.now(), 20*60 + 23)
+    #    self.assertEqual([g for g in used if g in algo._remainingUtopicGuards], used[0:2])
 
-        # First two used will go back, because they were unreachable since time
-        # 10 and 20, and now we are at GUARDS_RETRY_TIME + 23
-        self.assertEqual(simtime.now(), 20*60 + 23)
-        self.assertEqual([g for g in used if g in algo._remainingUtopicGuards], used[0:2])
-
-        self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
-        self.assertEqual(algo._triedGuards, used) # the 4th is now tried
-
+    #    self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
 
     def test_STATE_TRY_UTOPIC_returns_guard_from_REMAINING_UTOPIC_GUARDS(self):
         allGuards = [createGuard() for n in xrange(4)]
@@ -202,14 +185,12 @@ class TestProposal259(unittest.TestCase):
         allDystopic = []
 
         params = client.ClientParams()
-        params.GUARDS_TRY_THRESHOLD = 1
-        params.GUARDS_FAILOVER_THRESHOLD = 1
-
         algo = proposal.ChooseGuardAlgorithm(params)
         algo.start(used, [], [], [], 3, allGuards, allDystopic)
 
         chosen = algo.nextGuard()
 
+        # XXX this is not always TRY_UTOPIC, sometimes is PRIMARY_GUARDS, why?
         self.assertEqual(algo._state, algo.STATE_TRY_UTOPIC)
         self.assertEqual(len(algo._remainingUtopicGuards), 1)
         self.assertEqual(chosen, algo._remainingUtopicGuards.pop())
